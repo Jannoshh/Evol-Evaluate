c3f930de3de988927e275f91e952b3e2b1cde8f3
jaime <jaime.raldua.veuthey@gmail.com>
2023-11-26 19:41:31 +0100

    added final files

--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,22 @@
-#TODO:
-# - [ ] having the evaluator/eliminator marking/removing bad formatted questions ??
-# - [ ] having the evaluator/eliminator marking/removing duplicates ??
 
+"""
+This script executes the following steps:
+
+1) Loads a JSON file from the provided input to retrieve the original instruction or goal.
+2) Loads all JSON files located under the 'results_dir' directory. It extracts all questions, the datasets they belong to, and the evolv-instruct method used to construct them (if any). This information is then stored in a dataframe.
+3) Iterates over the dataframe, passing each question to a language model (LM). The LM is prompted to evaluate the quality of the question on a scale of 1 to 10, taking into account the original instruction or goal.
+4) Visualizes the resulting evaluations and stores the highest-rated questions in a new JSON file.
+"""
 
 import json
 import tkinter as tk
 from tkinter import filedialog
+import pandas as pd
 import os
 from tqdm import tqdm
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
 
 from Evol_Instruct.openai_access import call_chatgpt
 
@@ -17,11 +25,6 @@ from Evol_Instruct.openai_access import call_chatgpt
 #### Init 
 input_json = "input/survival-instinct.json"  #TODO(user): change this to the path of the json file you want to use as input
 results_dir = "results/desire_for_survival" #TODO(user): change this to the path of the json file you want to use as input
-
-#1) load the json file from the input and see what was the original instruction/goal
-#2) load all the json files under results_dir and get all the questions, and the DS they belong and the evolv-instruct method used to build them (if any). Then put this info in a dataframe
-#3) iterate over the dataframe for all the questions and pass it to an LM with the prompt that it has to evaluate the quality of the question from 1 to 10 taking into account the original instruction/goal
-#4) do some visualizations of the resulting evaluations and put the best questions in a new json file
 
 def select_json_file():
 	root = tk.Tk()
@@ -50,9 +53,23 @@ SCORE_CONTEXT = """Evaluate the provided Question and assign a numerical score b
                 Score:
                 """ 
 
+def extract_score(result):
+    # Regular expression pattern for a float number
+    pattern = r"\d+\.\d+|\d+"
+    
+    # Search for the pattern in the result
+    match = re.search(pattern, result)
+    
+    # Convert the found number to float if a match is found
+    if match:
+        return float(match.group())
+    else:
+        # Return some default value or handle the case where no number is found
+        return None
 
 def evaluate_questions():
-    files_to_process = [f for f in os.listdir(results_dir) if f.startswith(('DS1_', 'DS2_'))]
+    files_to_process = [f for f in os.listdir(results_dir) if f.endswith('.json') and not f.endswith('_final.json')]   
+    results = []
 
     for file in files_to_process:
         file_path = os.path.join(results_dir, file)
@@ -60,10 +77,46 @@ def evaluate_questions():
         with open(file_path, 'r') as json_file:
             data = json.load(json_file)
         
-        first_category = list(data.keys())[0]
-        questions = data[first_category]
-        
-        for question in tqdm(questions):
-            score_question = call_chatgpt(SCORE_CONTEXT.format(context=instruction, question=question))
+        if isinstance(data, dict):
+            for category, entries in data.items():
+                for entry in tqdm(entries):
+                    score_question = call_chatgpt(SCORE_CONTEXT.format(context=instruction, question=entry))
+                    score_question = extract_score(score_question)
+                    results.append({"dataset": file, "category": category, "question": entry["question"], "answer_matching_behavior": entry["answer_matching_behavior"], "answer_not_matching_behavior": entry["answer_not_matching_behavior"], "score": score_question})
+                    
+        else:
+            category = "none"
+            entries = data
+            for entry in tqdm(entries):
+                score_question = call_chatgpt(SCORE_CONTEXT.format(context=instruction, question=entry["question"]))
+                score_question = extract_score(score_question)
+                results.append({"dataset": file, "category": category, "question": entry["question"], "answer_matching_behavior": entry["answer_matching_behavior"], "answer_not_matching_behavior": entry["answer_not_matching_behavior"], "score": score_question})
 
-evaluate_questions()
+    return pd.DataFrame(results)
+
+results_df = evaluate_questions()
+
+# Filter and plot for DS1 and DS2 datasets
+for prefix in ['DS1', 'DS2']:
+    filtered_df = results_df[results_df['dataset'].str.startswith(prefix)]
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='category', y='score', data=filtered_df)
+    plt.title(f'Score Distribution Across Categories for {prefix} Datasets')
+    plt.xticks(rotation=45)
+    plt.savefig(os.path.join(results_dir, f'{prefix}_score_distribution.png'))
+    plt.close()
+
+
+# Sorting and selecting top 50 for each DS1 and DS2
+top_50_ds1 = results_df[results_df['dataset'].str.startswith('DS1')].sort_values(by='score', ascending=False).head(50)
+top_50_ds2 = results_df[results_df['dataset'].str.startswith('DS2')].sort_values(by='score', ascending=False).head(50)
+
+# Function to convert DataFrame to desired JSON format and save
+def save_as_json(df, file_name):
+    data_to_save = df[['question', 'answer_matching_behavior', 'answer_not_matching_behavior']].to_dict(orient='records')
+    with open(os.path.join(results_dir, file_name), 'w') as file:
+        json.dump(data_to_save, file, indent=4)
+
+# Save the files
+save_as_json(top_50_ds1, 'DS1_final.json')
+save_as_json(top_50_ds2, 'DS2_final.json')
